d73581685da9b347af7be9f365bacbaa580ef86b
Ori M. K <kosman.ori@gmail.com>
2021-02-06 17:19:22 +0200

    Add project files.

---
+++ SimpleBackupService/BackupProcess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Timers;
+
+namespace SimpleBackupService
+{
+    public class BackupProcess
+    {
+        private readonly Timer _timer;
+        string sourcePath = "C:/Users/OriKo/Downloads";
+        string targetPath = "C:/Users/OriKo/Documents/DocumentsFromDownloads";
+
+        public BackupProcess()
+        {
+            // Run every 2 hours
+            _timer = new Timer(7200000)
+            {
+                AutoReset = true
+            };
+
+            _timer.Elapsed += timer_Elapsed;
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(sourcePath))
+                    Directory.CreateDirectory(targetPath);
+
+                var files = Directory.EnumerateFiles(sourcePath, "*.*")
+                .Where(s => s.EndsWith(".pdf", System.StringComparison.OrdinalIgnoreCase)
+                        || s.EndsWith(".txt", System.StringComparison.OrdinalIgnoreCase)
+                        || s.EndsWith(".htm", System.StringComparison.OrdinalIgnoreCase));
+
+                foreach (var item in files)
+                {
+                    var fileName = Path.GetFileName(item);
+                    var sourceFile = Path.Combine(sourcePath, fileName);
+                    Console.WriteLine($"\nMoving file: {sourceFile}");
+
+                    var destFile = Path.Combine(targetPath, fileName);
+
+                    if (!File.Exists(destFile))
+                    {
+                        Console.WriteLine($"Destination: {destFile}");
+                        File.Move(item, destFile);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine($"Process failed: {exp}");
+            }
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ SimpleBackupService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SimpleBackupService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(host =>
+            {
+                host.Service<BackupProcess>(s =>
+                {
+                    s.ConstructUsing(backup => new BackupProcess());
+                    s.WhenStarted(backup => backup.Start());
+                    s.WhenStopped(backup => backup.Stop());
+                });
+
+                host.RunAsLocalSystem();
+                host.SetServiceName("SimpleBackupService");
+                host.SetDisplayName("My Simple Backup Service");
+                host.SetDescription("This simple backup service moves files from my Downloads folder to a folder in my Documents folder");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
